1598e2ad0d15ccb04114208693804cf0bdcca5d6
vinicius-rebelatto <vinicius-rebelatto@hotmail.com>
2024-11-25 15:46:10 -0300

    Main activit imported

---
+++ app/src/main/java/br/edu/up/rgm8829254761/data/OfflineItemsReporitory.kt
@@ -0,0 +1,3 @@
+package br.edu.up.rgm8829254761.data
+
+class OfflineItemsRepository : ItemsRepository
---
+++ app/src/main/java/br/edu/up/rgm8829254761/ui/AppViewModelProvider.kt
@@ -0,0 +1,50 @@
+package br.edu.up.rgm8829254761.ui
+
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import br.edu.up.rgm8829254761.InventoryApplication
+import br.edu.up.rgm8829254761.ui.home.HomeViewModel
+import br.edu.up.rgm8829254761.ui.item.ItemDetailsViewModel
+import br.edu.up.rgm8829254761.ui.item.ItemEditViewModel
+import br.edu.up.rgm8829254761.ui.item.ItemEntryViewModel
+
+/**
+ * Provides Factory to create instance of ViewModel for the entire Inventory app
+ */
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        // Initializer for ItemEditViewModel
+        initializer {
+            ItemEditViewModel(
+                this.createSavedStateHandle()
+            )
+        }
+        // Initializer for ItemEntryViewModel
+        initializer {
+            ItemEntryViewModel()
+        }
+
+        // Initializer for ItemDetailsViewModel
+        initializer {
+            ItemDetailsViewModel(
+                this.createSavedStateHandle()
+            )
+        }
+
+        // Initializer for HomeViewModel
+        initializer {
+            HomeViewModel()
+        }
+    }
+}
+
+/**
+ * Extension function to queries for [Application] object and returns an instance of
+ * [InventoryApplication].
+ */
+fun CreationExtras.inventoryApplication(): InventoryApplication =
+    (this[AndroidViewModelFactory.APPLICATION_KEY] as InventoryApplication)
